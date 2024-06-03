f1be87e3d442333a561f7dfdfac6a96438e7f663
aymanhajhamad <ayman.dhaj@gmail.com>
2024-06-03 12:37:09 +0300

    delete home controller

--- routes/web.php
+++ routes/web.php
@@ -2,5 +2,6 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/{number?}', [\App\Http\Controllers\HomeController::class,'index']);
-
+Route::get('/', function () {
+    return view('welcome');
+});
